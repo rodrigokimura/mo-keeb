90735f768081d123c0fedc92896845513ddeab72
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-04-03 21:31:12 -0300

    Listen to keyboard events

--- src/app.py
+++ src/app.py
@@ -1,13 +1,23 @@
 import os
 
+from pynput import keyboard
+
 from player import play
 
 
-def main():
+def on_press(key):
     path = os.getcwd()
-    print(path)
-    file = f"{path}/src/test.mp3"
+    file = f"{path}/src/test2.mp3"
     play(file)
+    try:
+        print("alphanumeric key {0} pressed".format(key.char))
+    except AttributeError:
+        print("special key {0} pressed".format(key))
+
+
+def main():
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
 
 
 if __name__ == "__main__":
