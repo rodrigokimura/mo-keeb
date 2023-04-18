0046bb64a39859c9b71d7d57ac43b70aa6bfe9f8
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-04-17 21:25:55 -0300

    Render pressed keys

--- src/app.py
+++ src/app.py
@@ -1,29 +1,55 @@
 import os
 
 import pygame
+from pynput import keyboard
+
+
+def callback(display: pygame.surface.Surface, *args, **kwargs):
+    key = args[0]
+    if isinstance(key, keyboard.KeyCode):
+        key = key.char
+    else:
+        key = key.name
+
+    display.fill("black")
+    font = pygame.font.SysFont("freesansbold", 48)
+    font_surf = font.render(key, True, "green")
+    display.blit(
+        font_surf,
+        (
+            display.get_rect().centerx - font_surf.get_rect().centerx,
+            display.get_rect().centery - font_surf.get_rect().centery,
+        ),
+    )
+
+    path = os.getcwd()
+    file = f"{path}/src/key1.wav"
+    sound = pygame.mixer.Sound(file)
+    sound.play()
 
 
 def main():
     pygame.init()
+    pygame.font.init()
     pygame.display.set_caption("mo-keeb")
-    size = [100, 100]
-    pygame.display.set_mode(size)
+    size = [200, 50]
+    display = pygame.display.set_mode(size)
+
+    clock = pygame.time.Clock()
+    listener = keyboard.Listener(
+        on_press=lambda *args, **kwargs: callback(display, *args, **kwargs),
+    )
+    listener.start()
 
     running = True
-    clock = pygame.time.Clock()
-    path = os.getcwd()
-    file = f"{path}/src/key1.wav"
-    sound = pygame.mixer.Sound(file)
-
     while running:
-        dt = clock.tick(100) / 1000
+        clock.tick(100)
 
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                sound.play()
-
             if event.type == pygame.QUIT:
                 running = False
+
+        display = pygame.display.get_surface()
 
         pygame.display.update()
 
