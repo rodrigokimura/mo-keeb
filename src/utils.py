efe18217e7da35eaa674b454236a051821a9e874
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-07-04 17:59:52 -0300

    Fix relative path of assets

--- src/utils.py
+++ src/utils.py
@@ -7,10 +7,8 @@ def is_windows():
 
 
 def get_asset_path(file_name: str):
-    root = os.getcwd()
-    if is_in_bunble():
-        return os.path.join(root, "assets", file_name)
-    return os.path.join(root, "src", "assets", file_name)
+    root = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(root, "assets", file_name)
 
 
 def is_in_bunble():
