5aff9c3dfbbdc7d2644859f394190b8841af5327
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-07-05 21:24:20 -0300

    Change to pygame-ce

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
-import pygame
+import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
 from config.models import Config
 from core.app import App
 
 if __name__ == "__main__":
+    import pygame
+
     config = Config.load()
     pygame.init()
     app = App(config)
